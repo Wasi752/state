ca31954ceacc0abc0e7774cc9bd75b53c612695e
Wasiur Rahman <osiur2@gmail.com>
2022-09-27 18:03:07 +0600

    Formating Page

--- src/Form.js
+++ src/Form.js
@@ -21,11 +21,11 @@ function Form(){
     return(
         <div className="w-full h-screen flex-col">
             <div className="w-full h-1/6"></div>
-            <div className="w-full h-4/6 flex">
+            <div className="w-full h-[70%] flex">
                 <div className="w-[25%] h-full"></div>    
                 <div className="w-[50%] h-full bg-gray-100 p-5 divide-x-2 divide-x-2">        
-          
-                    <div className="mt-20"><label className="ml-6 pr-2 ">User Name :</label>    
+                    <p className="mt-1 ml-5 text-left underline mr-10">Sign Up/Registration</p>
+                    <div className="mt-1"><label className="ml-6 pr-2 ">User Name :</label>    
                         <input 
                             type="text"
                             value= {name} 
@@ -61,7 +61,7 @@ function Form(){
                         <button 
                             type="button"
                             onClick={submit} 
-                            className="text-center text-2xl text-white font-bold h-[90%] w-[55%] ml-14 bg-blue-600 hover:text-blue-300 p-4 border rounded-md m-3">Submit</button>
+                            className="text-center text-2xl text-white font-bold h-[80%] w-[60%] ml-14 bg-blue-600 hover:text-blue-300 p-3 border rounded-md m-3">Submit</button>
                     </div>
                 </div>
                 <div className="w-[25%] h-full"></div>
--- src/Link.js
+++ src/Link.js
@@ -4,11 +4,11 @@ function Links() {
     return (
         <div className="w-full h-screen flex">
             <div className="w-[25%] h-full"></div>
-                <div className="flex flex-col justify-center m-5 w-[50%] h-[50%] mt-40 bg-gray-100">
-                    <Link to="/form" className="border p-5 m-5 text-3xl hover:text-blue-600">Form</Link>
-                    <Link to="/counter" className="border p-5 m-5 text-3xl hover:text-blue-600">Counter</Link>
-                    <Link to="/fetch" className="border p-5 m-5 text-3xl hover:text-blue-600">Fetch</Link>
-                </div>
+            <div className="flex flex-col justify-center m-5 w-[50%] h-[50%] mt-20 bg-gray-100">
+                    <Link to="/form" className="border border-blue-300 p-5 text-xl hover:text-blue-600">Form</Link>
+                    <Link to="/counter" className="border border-blue-300 p-5 text-xl hover:text-blue-600">Counter</Link>
+                    <Link to="/fetch" className="border border-blue-300 p-5 text-xl hover:text-blue-600">Fetch</Link>
+            </div>
             <div className="w-[25%] h-full"></div>
         </div>
     )
