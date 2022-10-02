18eb0619216f24a08b5fb34165b81906ca3d5a6c
Wasiur Rahman <osiur2@gmail.com>
2022-10-02 17:22:24 +0600

    Add Date

--- src/Queue.js
+++ src/Queue.js
@@ -1,28 +1,37 @@
 import { useState } from "react";
 
 function Queue(){
-    const [serial, setSerial]= useState(0);
+    const [serial, setSerial]= useState(2);
     const [name, setName]= useState();
     const [people, setPeople] = useState(["Nayeem", "Mahmud", "Mesbah", "Amin", "Rokon"]);
     return(
-        <div className="mt-40">
-            <button 
-            type="button"
-            onClick={()=> setSerial(serial + 1)}
-            className="text-center text-2xl text-white font-bold h-[80%] w-[56%] ml-14 bg-blue-600 hover:text-blue-300 p-3 border rounded-md m-3"
-            >Next</button>
+        <div className="flex flex-row w-full h-screen">
+                <div className="w-[25%] h-full"></div>
+                <div className="mt-40 flex flex-col w-[50%] h-full">
 
-            {people[serial]}
-            <input 
+                    {people[serial]}
+
+                    <input 
                             type="text" 
                             value= {name} 
                             onChange={e => setName(e.target.value)} 
-                            className="h-[10%] w-[50%] p-4 border border-blue-300 rounded-md m-3"
-                        />
-            <button 
+                            className="h-[9%] w-[60%] p-2 ml-40 m-3 border border-blue-300 rounded-md "
+                            />
+                    <button 
+                        type="button"
+                        onClick={()=> setPeople([...people, name])} 
+                        className="text-center text-2xl text-white font-bold h-[10%] w-[60%] ml-40 m-3 p-3 bg-blue-600 hover:text-blue-300 border rounded-md ">
+                        Submit
+                    </button>
+
+                    <button 
                             type="button"
-                            onClick={()=> setPeople([...people, name])} 
-                            className="text-center text-2xl text-white font-bold h-[80%] w-[56%] ml-14 bg-blue-600 hover:text-blue-300 p-3 border rounded-md m-3">Submit</button>
+                            onClick={()=> setSerial(serial + 1)}
+                            className="text-center text-2xl text-white font-bold h-[10%] w-[60%] m-3 ml-40 bg-blue-600 hover:text-blue-300 p-3 border rounded-md"
+                            >Next
+                    </button>
+                </div>
+                <div className="w-[25%] h-full"></div>
         </div>
     );
 }
